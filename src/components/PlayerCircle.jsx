84b2d3f620bc545d1c079484fb441b9444df7329
kevindantas <kevin.ds.29@gmail.com>
2016-09-02 10:17:56 -0300

    Animated player icons

---
+++ src/components/PlayerCircle.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CirclePlayer = () => (
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" className="player-mark"
+       x="0px" y="0px" width="149.8px" height="149.8px" viewBox="0 0 149.8 149.8" enableBackground="new 0 0 149.8 149.8">
+    <circle fill="none" stroke="#607D8B" strokeWidth="20" strokeMiterlimit="10" cx="74.9" cy="74.9" r="64.9"/>
+  </svg>
+);
+
+export default CirclePlayer;
---
+++ src/components/PlayerCross.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PlayerCross = () => (
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="153.3px" height="153.3px" viewBox="0 0 153.3 153.3" className="player-mark">
+    <line fill="none" stroke="#607D8B" strokeWidth="20" strokeMiterlimit="10" x1="7.1" y1="146.2" x2="146.2" y2="7.1"/>
+    <line fill="none" stroke="#607D8B" strokeWidth="20" strokeMiterlimit="10" x1="7.1" y1="7.1" x2="146.2" y2="146.2"/>
+  </svg>
+);
+
+export default PlayerCross;
