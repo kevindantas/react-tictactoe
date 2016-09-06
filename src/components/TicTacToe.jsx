1110cd8743d7c280a17327ffcbc023fd12b1547e
kevindantas <kevin.ds.29@gmail.com>
2016-09-06 17:09:41 -0300

    Mark fields

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -10,7 +10,12 @@ class TicTacToe extends Component {
 
     this.state = {
       gameMode: null
-    }
+    };
+
+    this.state = {
+      fields: (new Array(9)).fill(null),
+      player: 1
+    };
   }
 
 
@@ -21,12 +26,141 @@ class TicTacToe extends Component {
   }
 
 
+
+  togglePlayer() {
+    var player;
+    if(this.state.player == 1)
+      player = 2;
+     else
+      player = 1;
+
+    this.setState({
+      player: player
+    });
+  }
+
+
+  /**
+   * Field click handler
+   *
+   * @param {int} index
+   * @param {proxy} e
+   *
+   * @returns {number}
+   */
+  fieldClickHandle(index, e) {
+
+    var fields = this.state.fields;
+        fields[index] = this.state.player;
+
+    this.setState({
+      fields: fields
+    });
+
+
+    this.togglePlayer();
+
+    this.checkWinner();
+    
+    return this.state.player;
+  }
+
+
+
+  /**
+   * Check who's the winner
+   *
+   * @returns {int}
+   */
+  checkWinner() {
+    const victoriesCombos = [
+      {
+        player1: 0,
+        player2:0,
+        fields: [0,1,2]
+      },
+      {
+        player1: 0,
+        player2:0,
+        fields: [3,4,5]
+      },
+      {
+        player1: 0,
+        player2:0,
+        fields: [6,7,8]
+      },
+
+      {
+        player1: 0,
+        player2:0,
+        fields: [0,3,6]
+      },
+      {
+        player1: 0,
+        player2:0,
+        fields: [1,4,5]
+      },
+      {
+        player1: 0,
+        player2:0,
+        fields: [2,5,8]
+      },
+
+      {
+        player1: 0,
+        player2:0,
+        fields: [0,4,8]
+      },
+      {
+        player1: 0,
+        player2:0,
+        fields: [2,4,6]
+      },
+    ];
+
+    var combos;
+
+    for (let i = 0; i < this.state.fields.length; i++) {
+      var field = this.state.fields[i];
+      combos = victoriesCombos.map(combo => {
+        if(combo.fields.indexOf(field) > -1)
+          combo[`player${field}`]++;
+
+        return combo;
+      });
+    }
+    
+    combos.every((combo, a) => {
+      console.log(combo);
+      console.log(a);
+      return true;
+    });
+
+
+    this.combos = combos;
+
+
+    this.showWinner();
+  }
+
+
+  showWinner() {
+    
+  }
+
+
+
+  /**
+   * Render each field
+   *
+   * @returns {*}
+   */
   renderFields() {
     if(this.state.gameMode){
       this.fieldsArr = (new Array(9).fill(null));
 
       this.fields = this.fieldsArr.map((field, i) => {
-        return (<TicTacToeField key={i} index={i} />);
+        return (<TicTacToeField key={i} index={i} fieldClick={this.fieldClickHandle.bind(this, i)} />);
       });
 
       return this.fields;
@@ -36,6 +170,10 @@ class TicTacToe extends Component {
   }
 
 
+  /**
+   * Render Component
+   * @returns {DOM}
+   */
   render() {
     return (
       <div className="tictactoe">
--- src/components/TicTacToeField.jsx
+++ src/components/TicTacToeField.jsx
@@ -1,10 +1,37 @@
-import React, {Component} from 'react';
+import React, {PropTypes, Component} from 'react';
 
 import FlatButton from 'material-ui/FlatButton';
 
+import PlayerCircle from './PlayerCircle.jsx';
+import PlayerCross from './PlayerCross.jsx';
+
 export default class TicTacToeField extends Component {
 
-  
+  constructor() {
+    super();
+
+    this.state = {
+      icon: null
+    };
+  }
+
+
+  onClickHandle() {
+    
+    const player = this.props.fieldClick();
+
+    if(player == 1) {
+      this.setState({
+        icon: <PlayerCircle />
+      })
+    } else {
+      this.setState({
+        icon: <PlayerCross />
+      })
+    }
+
+  }
+
   
   render() {
     const buttonStyle =  {
@@ -19,7 +46,15 @@ export default class TicTacToeField extends Component {
         label="&nbsp;"
         style={buttonStyle}
         rippleColor="#CDDC39"
+        icon={this.state.icon}
+        disabled={!!this.state.icon}
+        onClick={this.onClickHandle.bind(this)}
         className={`field field-${this.props.index}`} />
     );
   }
 }
+
+
+TicTacToeField.props = {
+  fieldClick: PropTypes.func
+};
